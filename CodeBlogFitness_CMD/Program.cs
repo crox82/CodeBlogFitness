17dc2570589d043dbb4484774e8da16b7fd43fb0
Шалигін Євген <yevhen.shalyhin@volia.com>
2019-10-29 14:10:54 +0200

    Добавьте файлы проекта.

---
+++ CodeBlogFitness_CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeBlogFitness_CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
